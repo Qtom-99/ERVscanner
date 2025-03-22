66629ff44de08087a25efd0abc0e634333387e7b
Qtom-99 <9tom75@gmail.com>
2025-03-22 16:30:29 +0900

    Update pipeline1.sh

--- pipeline/pipeline1.sh
+++ pipeline/pipeline1.sh
@@ -29,8 +29,8 @@ date
 echo "=== process2: SAMPLE:${line} insertion推定開始 ==="
 samtools view -@ $NCORE -b -F 256 $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap.bam |samtools view -@ $NCORE -F 2048 -o $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048.bam -
 samtools sort -@ $NCORE -n -o $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048_sortn.bam $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048.bam
-bedtools pairtobed -type xor -abam $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048_sortn.bam -b $QUERY_BED_FULLPATH 2>/dev/null > /media/fujiwara/WD181PURP18Tb/yano24_08/mouse/benchmark/yano/5reads_q30/sampledata/JF1_cov30_reheader/${line}_dfamallhit_overlap_F256_F2048_sortn_xor.bam
-bedtools bamtobed -i /media/fujiwara/WD181PURP18Tb/yano24_08/mouse/benchmark/yano/5reads_q30/sampledata/JF1_cov30_reheader/${line}_dfamallhit_overlap_F256_F2048_sortn_xor.bam > $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048_sortn_xor.bed
+bedtools pairtobed -type xor -abam $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048_sortn.bam -b $QUERY_BED_FULLPATH 2>/dev/null > $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048_sortn_xor.bam
+bedtools bamtobed -i $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048_sortn_xor.bam > $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048_sortn_xor.bed
 awk -F'\t' '$4 ~ "/1$"' $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048_sortn_xor.bed > $DATA_PATH/sampledata/${line}/${line}_read1.bed
 awk -F'\t' '$4 ~ "/2$"' $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048_sortn_xor.bed > $DATA_PATH/sampledata/${line}/${line}_read2.bed
 python3 $DATA_PATH/script/bed_curate.py $DATA_PATH/sampledata/${line}/${line}_read1.bed $DATA_PATH/sampledata/${line}/${line}_read2.bed $DATA_PATH/sampledata/${line}/${line}_pair.bed
@@ -41,7 +41,7 @@ cat $DATA_PATH/sampledata/${line}/${line}_other_chr.bed $DATA_PATH/sampledata/${
 cut -f 4 $DATA_PATH/sampledata/${line}/${line}_mateunmapped.bed > $DATA_PATH/sampledata/${line}/${line}_readname.txt
 python3 $DATA_PATH/script/remove_endname.py $DATA_PATH/sampledata/${line}/${line}_readname.txt $DATA_PATH/sampledata/${line}/${line}_readname_nonread.txt
 sort $DATA_PATH/sampledata/${line}/${line}_readname_nonread.txt | uniq - > $DATA_PATH/sampledata/${line}/${line}_readname_nonread_uniq.txt
-samtools view -@ $NCORE -N $DATA_PATH/sampledata/${line}/${line}_readname_nonread_uniq.txt -o $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048_sortn_xor_discordant.bam /media/fujiwara/WD181PURP18Tb/yano24_08/mouse/benchmark/yano/5reads_q30/sampledata/JF1_cov30_reheader/${line}_dfamallhit_overlap_F256_F2048_sortn_xor.bam
+samtools view -@ $NCORE -N $DATA_PATH/sampledata/${line}/${line}_readname_nonread_uniq.txt -o $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048_sortn_xor_discordant.bam $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048_sortn_xor.bam
 bedtools bamtobed -i $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048_sortn_xor_discordant.bam > $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048_sortn_xor_discordant.bed
 bedtools intersect -v -abam $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048_sortn_xor_discordant.bam -b $QUERY_BED_FULLPATH > $DATA_PATH/sampledata/${line}/${line}_dfamallhit_ERV_bed_position_F256_F2048_sortn_xor_discordant_onlyf8.bam
 samtools sort -@ $NCORE -o $DATA_PATH/sampledata/${line}/${line}_dfamallhit_ERV_bed_position_F256_F2048_sortn_xor_discordant_onlyf8_sort.bam $DATA_PATH/sampledata/${line}/${line}_dfamallhit_ERV_bed_position_F256_F2048_sortn_xor_discordant_onlyf8.bam
@@ -64,14 +64,14 @@ bedtools intersect -a $DATA_PATH/sampledata/${line}/curated_bam/${line}_dfamallh
 cut -f 4 $DATA_PATH/sampledata/${line}/read_info/${line}_${CLUSTER_THRESHOLD}reads_in_cluster.bed > $DATA_PATH/sampledata/${line}/read_info/${line}_posreadname.txt
 python3 $DATA_PATH/script/remove_endname.py $DATA_PATH/sampledata/${line}/read_info/${line}_posreadname.txt $DATA_PATH/sampledata/${line}/read_info/${line}_posreadname_nonend.txt
 uniq $DATA_PATH/sampledata/${line}/read_info/${line}_posreadname_nonend.txt > $DATA_PATH/sampledata/${line}/read_info/${line}_posreadname_nonend_uniq
-bedtools intersect -a /media/fujiwara/WD181PURP18Tb/yano24_08/mouse/benchmark/yano/5reads_q30/sampledata/JF1_cov30_reheader/${line}_dfamallhit_overlap_F256_F2048_sortn_xor.bam -b $QUERY_BED_FULLPATH -v > $DATA_PATH/sampledata/${line}/read_info/${line}_ERVposition.bam
+bedtools intersect -a $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048_sortn_xor.bam -b $QUERY_BED_FULLPATH -v > $DATA_PATH/sampledata/${line}/read_info/${line}_ERVposition.bam
 samtools view -@ $NCORE -F 256 -b $DATA_PATH/sampledata/${line}/read_info/${line}_ERVposition.bam | samtools view -@ $NCORE -b -F 2048 -o $DATA_PATH/sampledata/${line}/read_info/${line}_ERVposition_F256_F2048.bam -
 samtools view -@ $NCORE -N $DATA_PATH/sampledata/${line}/read_info/${line}_posreadname_nonend_uniq -o $DATA_PATH/sampledata/${line}/read_info/${line}_ERVposition_read_true.bam $DATA_PATH/sampledata/${line}/read_info/${line}_ERVposition_F256_F2048.bam
 samtools view -@ $NCORE -o $DATA_PATH/sampledata/${line}/read_info/${line}_ERVposition_read_true.sam $DATA_PATH/sampledata/${line}/read_info/${line}_ERVposition_read_true.bam
 cut -f 1,3,4,7,8 $DATA_PATH/sampledata/${line}/read_info/${line}_ERVposition_read_true.sam > $DATA_PATH/sampledata/${line}/read_info/${line}_ERVposition_read_true_cut.sam
 awk 'BEGIN{FS=OFS="\t"} $4=="="{ $4=$2 }1' $DATA_PATH/sampledata/${line}/read_info/${line}_ERVposition_read_true_cut.sam > $DATA_PATH/sampledata/${line}/read_info/${line}_ERVposition_read_true_cut_awk.sam
 sort -V -k2,2 -k3,3 $DATA_PATH/sampledata/${line}/read_info/${line}_ERVposition_read_true_cut_awk.sam > $DATA_PATH/sampledata/${line}/read_info/${line}
-bedtools intersect -a /media/fujiwara/WD181PURP18Tb/yano24_08/mouse/benchmark/yano/5reads_q30/sampledata/JF1_cov30_reheader/${line}_dfamallhit_overlap_F256_F2048_sortn_xor.bam -b $QUERY_BED_FULLPATH -wa > $DATA_PATH/sampledata/${line}/read_info/${line}_ERVread.bam
+bedtools intersect -a $DATA_PATH/sampledata/${line}/${line}_dfamallhit_overlap_F256_F2048_sortn_xor.bam -b $QUERY_BED_FULLPATH -wa > $DATA_PATH/sampledata/${line}/read_info/${line}_ERVread.bam
 samtools view -@ $NCORE -F 256 -b $DATA_PATH/sampledata/${line}/read_info/${line}_ERVread.bam | samtools view -@ $NCORE -b -F 2048 -o $DATA_PATH/sampledata/${line}/read_info/${line}_ERVread_F256_F2048.bam -
 samtools view -@ $NCORE -N $DATA_PATH/sampledata/${line}/read_info/${line}_posreadname_nonend_uniq -o $DATA_PATH/sampledata/${line}/read_info/${line}_ERVread_true.bam $DATA_PATH/sampledata/${line}/read_info/${line}_ERVread_F256_F2048.bam
 samtools sort -@ $NCORE -n -o $DATA_PATH/sampledata/${line}/read_info/${line}_ERVread_true_sort.bam $DATA_PATH/sampledata/${line}/read_info/${line}_ERVread_true.bam
