a44d86e95cb80610debc04bf98fda215b90388f1
nosada17 <35247476+nosada17@users.noreply.github.com>
2025-03-26 15:27:49 +0900

    Update identify_loci.sh

--- pipeline/identify_loci.sh
+++ pipeline/identify_loci.sh
@@ -18,12 +18,12 @@ if [[ -z "$SAMPLE" || -z "$DATA_PATH" ]]; then
 fi
 
 date
-echo "=== process6: 各サンプルのinsertionとポジションIDを照合中 ==="
+echo "=== process6: checking ID of insertions ==="
 while read line
 do
 python3 $DATA_PATH/script/find_POS_key4.py $DATA_PATH/sampledata/$line/read_info/${line} $DATA_PATH/allsample_merge/POSITION_LIST $DATA_PATH/sampledata/$line/read_info/${line}_data.csv $NCORE
 python3 $DATA_PATH/script/add_strand.py $DATA_PATH/sampledata/$line/read_info/${line}_data.csv $DATA_PATH/sampledata/$line/read_info/${line}_*reads_in_cluster.bed > $DATA_PATH/sampledata/$line/read_info/${line}_data_strand.csv
 cp $DATA_PATH/sampledata/$line/read_info/${line}_data_strand.csv $DATA_PATH/check_seq/master_file/sampledata/.
-echo "=== process6: ${line}の照合完了 ==="
+echo "=== process6: checking ID of insertions: completed ${line} ==="
 done < $SAMPLE
-echo "=== process6: 全サンプルの照合完了 ==="
+echo "=== process6: checking ID of insertions: finished ==="
