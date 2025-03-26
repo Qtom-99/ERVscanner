63133fa17378adcc33b93bacf7bccb1d8a33f7ec
nosada17 <35247476+nosada17@users.noreply.github.com>
2025-03-26 18:39:18 +0900

    Update extract_sample_pos.py

--- python/extract_sample_pos.py
+++ python/extract_sample_pos.py
@@ -3,7 +3,7 @@
 import sys
 
 def main(sample_name, mutations_tsv, positions_bed, output_bed):
-    # サンプルが持つ変異のポジション名を取得
+    # Retrieve the position names of variants present in the sample
     mutation_positions = set()
     with open(mutations_tsv, 'r') as tsv_file:
         lines = tsv_file.readlines()
@@ -18,7 +18,7 @@ def main(sample_name, mutations_tsv, positions_bed, output_bed):
                         mutation_positions.add(headers[i])
                 break
         if not sample_found:
-            print(f'サンプル "{sample_name}" が {mutations_tsv} に見つかりませんでした。')
+            print(f'Sample "{sample_name}" is not found in {mutations_tsv}')
             sys.exit(1)
 
     # BEDファイルから該当するポジションを抽出
@@ -28,11 +28,11 @@ def main(sample_name, mutations_tsv, positions_bed, output_bed):
             if len(fields) >= 4 and fields[3] in mutation_positions:
                 out_file.write(line)
 
-    print(f'サンプル "{sample_name}" の変異のみを含むBEDファイル "{output_bed}" を作成しました。')
+    print(f'BED file "{output_bed}" with the variants of sample "{sample_name}" was generated')
 
 if __name__ == '__main__':
     if len(sys.argv) != 5:
-        print('使用法: python script.py サンプル名 mutations.tsv positions.bed 出力ファイル.bed')
+        print('usage: python script.py sample_name mutations.tsv positions.bed output.bed')
         sys.exit(1)
     sample_name = sys.argv[1]
     mutations_tsv = sys.argv[2]
