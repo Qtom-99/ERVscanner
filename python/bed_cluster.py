0bd5dd243af09ff282aa97e3246d13706f40ad6e
nosada17 <35247476+nosada17@users.noreply.github.com>
2025-03-26 16:38:37 +0900

    Update bed_cluster.py

--- python/bed_cluster.py
+++ python/bed_cluster.py
@@ -2,17 +2,17 @@ import sys
 import pandas as pd
 
 def filter_file(input_file_path, output_file_path, min_count):
-    # ファイルの読み込み
+    # reading file
     df = pd.read_csv(input_file_path, sep='\t', header=None)
 
-    # 7列目の値を基準に行をフィルタリング
+    # Filter rows based on the value in the 7th column.
     filtered_df = df[df.groupby(6)[6].transform('count') >= min_count]
 
     # 結果をファイルに書き込み
     filtered_df.to_csv(output_file_path, sep='\t', index=False, header=False)
 
 if __name__ == "__main__":
-    # コマンドライン引数の取得
+    # Get command-line arguments
     if len(sys.argv) != 4:
         print("Usage: python bed_curate.py input_file.tsv output_file.tsv min_count")
         sys.exit(1)
@@ -25,5 +25,5 @@ if __name__ == "__main__":
         print("min_count must be an integer.")
         sys.exit(1)
 
-    # ファイルのフィルタリング
+    # filtering file
     filter_file(input_file, output_file, min_count)
