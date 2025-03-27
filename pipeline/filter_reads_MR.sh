46d552aeba8edce0f2c9e0b627c5e67ae8cc695c
nosada17 <35247476+nosada17@users.noreply.github.com>
2025-03-27 10:44:55 +0900

    Update filter_reads_MR.sh

--- pipeline/filter_reads_MR.sh
+++ pipeline/filter_reads_MR.sh
@@ -23,8 +23,9 @@ done
 
 REF_GENOME="$DATA_PATH/reference/reference.fasta"
 QUERY_BED="$DATA_PATH/dfam_info/target.bed"
+ALT_CHR_LIST=$DATA_PATH/reference/alt_chr_list
 
-if [[ -z "$SAMPLE" || -z "$INPUT_PATH" || -z "$DATA_PATH" || -z "$ALT_CHR_LIST" ]]; then
+if [[ -z "$SAMPLE" || -z "$INPUT_PATH" || -z "$DATA_PATH" ]]; then
   echo "Error: requied option values are missing" >&2
   exit 1
 fi
