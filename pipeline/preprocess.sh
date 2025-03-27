a792d14ff4601bf6762384308ccb31d19757d959
nosada17 <35247476+nosada17@users.noreply.github.com>
2025-03-27 10:40:28 +0900

    Update preprocess.sh

--- pipeline/preprocess.sh
+++ pipeline/preprocess.sh
@@ -12,7 +12,7 @@ while getopts "i:s:r:h:d:n:b:q:c:a:p:" opt; do
     n) NRPH="$OPTARG" ;;
     h) TARGET="$OPTARG" ;;
     a) ALT_CHR_LIST="$OPTARG" ;;
-    p) PY_PATH="$OPTARG" ;;
+    p) PY_PATH="$OPTARG"/script ;;
     \?) echo "Usage: $0 [-i input] [-o output]" >&2; exit 1 ;;
   esac
 done
