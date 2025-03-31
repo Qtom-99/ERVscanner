0e1d09242b92593d36b83e138cc9fc1ed4903b1b
nosada17 <35247476+nosada17@users.noreply.github.com>
2025-03-31 18:11:52 +0900

    Update filter_reads.sh

--- pipeline/filter_reads.sh
+++ pipeline/filter_reads.sh
@@ -28,6 +28,10 @@ eval $(awk -F: '
     {print $1"=\"" $2 "\""}
 ' "$CONFIG_FILE")
 
+REF_GENOME="$DATA_PATH/reference/reference.fasta"
+QUERY_BED="$DATA_PATH/dfam_info/target.bed"
+ALT_CHR_LIST=$DATA_PATH/reference/alt_chr_list
+
 while read line
 do
 date
