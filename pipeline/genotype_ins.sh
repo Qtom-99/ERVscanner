71c3dd3f43da3c8e028c5c82a4ef9ca9dcd7b953
DW8496 <96272772+DW8496@users.noreply.github.com>
2025-03-31 15:11:24 +0900

    Update genotype_ins.sh

--- pipeline/genotype_ins.sh
+++ pipeline/genotype_ins.sh
@@ -3,15 +3,19 @@
 # default values
 INPUT_TYPE="bam"
 NCORE=1
+threshold="0.7"
+QUALITY="30"
 
 # getting option values
-while getopts "i:s:t:d:n:" opt; do
+while getopts "i:s:t:d:n:m:q:" opt; do
   case $opt in
     s) SAMPLE="$OPTARG" ;;
     i) INPUT_PATH="$OPTARG" ;;
     t) INPUT_TYPE="$OPTARG" ;;
     d) DATA_PATH="$OPTARG" ;;
     n) NCORE="$OPTARG" ;;
+    m) threshold="$OPTARG" ;;
+    q) QUALITY="$OPTARG" ;;
     \?) echo "Usage: $0 [-i input] [-o output]" >&2; exit 1 ;;
   esac
 done
@@ -51,16 +55,16 @@ bedtools cluster -d 50 -i $DATA_PATH/sampledata/${line}/clip/${line}_clipped_rea
 python3 $DATA_PATH/script/bed_cluster_plusminus.py $DATA_PATH/sampledata/${line}/clip/${line}_clipped_reads_sort_merged50_clustered50.bed $DATA_PATH/sampledata/${line}/clip/${line}_clipped_reads_sort_merged50_clustered50_plusminus.bed
 python3 $DATA_PATH/script/bed_cluster_plusminus_jun.py $DATA_PATH/sampledata/${line}/clip/${line}_clipped_reads_sort_merged50_clustered50_plusminus.bed $DATA_PATH/sampledata/${line}/clip/${line}_clipped_reads_sort_merged50_clustered50_plusminus_jun.bed
 python3 $DATA_PATH/script/get_center_pos.py $DATA_PATH/sampledata/${line}/clip/${line}_clipped_reads_sort_merged50_clustered50_plusminus_jun.bed $DATA_PATH/sampledata/${line}/clip/${line}_clipped_reads_sort_merged50_clustered50_plusminus_jun_centerpos.txt
-python3 $DATA_PATH/script/extract_sample_pos.py ${line} $DATA_PATH/01table/allsample_01table_07up.tsv $DATA_PATH/allsample_merge/pos.bed $DATA_PATH/sampledata/${line}/clip/${line}_pos.bed
+python3 $DATA_PATH/script/extract_sample_pos.py ${line} $DATA_PATH/01table/allsample_01table_${threshold//./}up.tsv $DATA_PATH/allsample_merge/pos.bed $DATA_PATH/sampledata/${line}/clip/${line}_pos.bed
 python3 $DATA_PATH/script/match_centerpos_to_POS.py $DATA_PATH/sampledata/${line}/clip/${line}_clipped_reads_sort_merged50_clustered50_plusminus_jun_centerpos.txt $DATA_PATH/sampledata/${line}/clip/${line}_pos.bed $DATA_PATH/sampledata/${line}/clip/${line}_breakpoint.tsv
 samtools view -@ "$NCORE" -T "$REF_GENOME" -L $DATA_PATH/sampledata/${line}/clip/${line}_pos.bed -o "$DATA_PATH"/genotype/sampledata/${line}/${line}_muttype.bam -O BAM "$INPUT_PATH"/${line}."$INPUT_TYPE"
-samtools view -@ $NCORE -q 30 -o "$DATA_PATH"/genotype/sampledata/${line}/${line}_muttype_q30.bam "$DATA_PATH"/genotype/sampledata/${line}/${line}_muttype.bam
-samtools view -@ $NCORE -h $DATA_PATH/genotype/sampledata/${line}/${line}_muttype_q30.bam | awk '$6 !~ /[SH]/ || $1 ~ /^@/' | samtools view -b -o $DATA_PATH/genotype/sampledata/${line}/${line}_q30_no_clipping.bam -
-bedtools bamtobed -i $DATA_PATH/genotype/sampledata/${line}/${line}_q30_no_clipping.bam > $DATA_PATH/genotype/sampledata/${line}/${line}_q30_no_clipping.bed
-sort -V -k1,1 -k2,2 $DATA_PATH/genotype/sampledata/${line}/${line}_q30_no_clipping.bed > $DATA_PATH/genotype/sampledata/${line}/${line}_q30_no_clipping_sort.bed
-cut -f 1,2,3 $DATA_PATH/genotype/sampledata/${line}/${line}_q30_no_clipping_sort.bed > $DATA_PATH/genotype/sampledata/${line}/${line}_q30_no_clipping_sort123.bed
+samtools view -@ $NCORE -q 30 -o "$DATA_PATH"/genotype/sampledata/${line}/${line}_muttype_q${QUALITY}.bam "$DATA_PATH"/genotype/sampledata/${line}/${line}_muttype.bam
+samtools view -@ $NCORE -h $DATA_PATH/genotype/sampledata/${line}/${line}_muttype_q${QUALITY}.bam | awk '$6 !~ /[SH]/ || $1 ~ /^@/' | samtools view -b -o $DATA_PATH/genotype/sampledata/${line}/${line}_q${QUALITY}_no_clipping.bam -
+bedtools bamtobed -i $DATA_PATH/genotype/sampledata/${line}/${line}_q${QUALITY}_no_clipping.bam > $DATA_PATH/genotype/sampledata/${line}/${line}_q${QUALITY}_no_clipping.bed
+sort -V -k1,1 -k2,2 $DATA_PATH/genotype/sampledata/${line}/${line}_q${QUALITY}_no_clipping.bed > $DATA_PATH/genotype/sampledata/${line}/${line}_q${QUALITY}_no_clipping_sort.bed
+cut -f 1,2,3 $DATA_PATH/genotype/sampledata/${line}/${line}_q${QUALITY}_no_clipping_sort.bed > $DATA_PATH/genotype/sampledata/${line}/${line}_q${QUALITY}_no_clipping_sort123.bed
 awk -v OFS="\t" '{print $1, $2-15, $2+15, $4}' $DATA_PATH/sampledata/${line}/clip/${line}_breakpoint.tsv > $DATA_PATH/sampledata/${line}/clip/${line}_breakpoint.bed
-bedtools intersect -a $DATA_PATH/sampledata/${line}/clip/${line}_breakpoint.bed -b $DATA_PATH/genotype/sampledata/${line}/${line}_q30_no_clipping_sort123.bed -f 1.0 -c > $DATA_PATH/genotype/sampledata/$line/${line}_n_of_covreads_noclip.txt
+bedtools intersect -a $DATA_PATH/sampledata/${line}/clip/${line}_breakpoint.bed -b $DATA_PATH/genotype/sampledata/${line}/${line}_q${QUALITY}_no_clipping_sort123.bed -f 1.0 -c > $DATA_PATH/genotype/sampledata/$line/${line}_n_of_covreads_noclip.txt
 cp $DATA_PATH/genotype/sampledata/$line/${line}_n_of_covreads_noclip.txt $DATA_PATH/genotype/allsample/.
 date
 echo "=== process8: SAMPLE:${line} genotyping: finished  ==="
