6e699cfafdc33556377580d9de5baec7810f561a
nosada17 <35247476+nosada17@users.noreply.github.com>
2025-03-31 18:28:14 +0900

    Update identify_loci.sh

--- pipeline/identify_loci.sh
+++ pipeline/identify_loci.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
-# default values
-NCORE=1
+# get config file
+CONFIG_FILE="$1"
 
-# getting option values
-while getopts "d:s:n:" opt; do
-  case $opt in
-    s) SAMPLE="$OPTARG" ;;
-    d) DATA_PATH="$OPTARG" ;;
-    \?) echo "Usage: $0 [-i input] [-o output]" >&2; exit 1 ;;
-  esac
-done
-
-if [[ -z "$SAMPLE" || -z "$DATA_PATH" ]]; then
-  echo "Error: requied option values are missing" >&2
-  exit 1
+# checking the existence of config file
+if [ -z "$CONFIG_FILE" ]; then
+    echo "Error: Configuration file not specified."
+    echo "Usage: $0 <config_file>"
+    exit 1
 fi
 
-# chekcing file does exist
-if [[ ! -f "$SAMPLE" ]]; then
-  echo "Error: File '$SAMPLE' not found." >&2
-  exit 1
+#  checking the existence of config file
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "Error: Configuration file '$CONFIG_FILE' not found."
+    exit 1
 fi
 
-if [[ ! -d "$DATA_PATH" ]]; then
-  echo "Error: Directory '$DATA_PATH' not found." >&2
-  exit 1
-fi
+# reading confing file
+eval $(awk -F: '
+    /^[[:space:]]*$/ {next}      # skip empty line
+    /^[[:space:]]*#/ {next}      # ignore comment line
+
+    # remove spaces
+    {gsub(/^[ \t]+|[ \t]+$/, "", $1); gsub(/^[ \t]+|[ \t]+$/, "", $2)}
+
+    # exporting parameters
+    {print $1"=\"" $2 "\""}
+' "$CONFIG_FILE")
 
 date
 echo "=== process6: checking ID of insertions ==="
 while read line
 do
-python3 $DATA_PATH/script/find_POS_key4.py $DATA_PATH/sampledata/$line/read_info/${line} $DATA_PATH/allsample_merge/POSITION_LIST $DATA_PATH/sampledata/$line/read_info/${line}_data.csv $NCORE
-python3 $DATA_PATH/script/add_strand.py $DATA_PATH/sampledata/$line/read_info/${line}_data.csv $DATA_PATH/sampledata/$line/read_info/${line}_*reads_in_cluster.bed > $DATA_PATH/sampledata/$line/read_info/${line}_data_strand.csv
+python3 $PY_PATH/find_POS_key4.py $DATA_PATH/sampledata/$line/read_info/${line} $DATA_PATH/allsample_merge/POSITION_LIST $DATA_PATH/sampledata/$line/read_info/${line}_data.csv $NCORE
+python3 $PY_PATH/add_strand.py $DATA_PATH/sampledata/$line/read_info/${line}_data.csv $DATA_PATH/sampledata/$line/read_info/${line}_*reads_in_cluster.bed > $DATA_PATH/sampledata/$line/read_info/${line}_data_strand.csv
 cp $DATA_PATH/sampledata/$line/read_info/${line}_data_strand.csv $DATA_PATH/check_seq/master_file/sampledata/.
 echo "=== process6: checking ID of insertions: completed ${line} ==="
 done < $SAMPLE
