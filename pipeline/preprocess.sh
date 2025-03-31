aa3f3dbfbf3aa23c2cdce1f9a363080cb806db18
nosada17 <35247476+nosada17@users.noreply.github.com>
2025-03-31 18:03:35 +0900

    Update preprocess.sh

--- pipeline/preprocess.sh
+++ pipeline/preprocess.sh
@@ -1,62 +1,32 @@
 #!/bin/bash
 
-# default values
+# get config file
+CONFIG_FILE="$1"
 
-# getting option values
-while getopts "s:r:d:f:n:h:a:p:" opt; do
-  case $opt in
-    s) SAMPLE="$OPTARG" ;;
-    r) REF_GENOME="$OPTARG" ;;
-    d) DATA_PATH="$OPTARG" ;;
-    f) DFAM_INFO="$OPTARG" ;;
-    n) NRPH="$OPTARG" ;;
-    h) TARGET="$OPTARG" ;;
-    a) ALT_CHR_LIST="$OPTARG" ;;
-    p) PY_PATH="$OPTARG"/python ;;
-    \?) echo "Usage: $0 [-i input] [-o output]" >&2; exit 1 ;;
-  esac
-done
-
-# checking all required options are specified
-if [[ -z "$SAMPLE" || -z "$REF_GENOME" || -z "$DATA_PATH" || -z "$DFAM_INFO" || -z "$ALT_CHR_LIST" || -z "$NRPH" || -z "$TARGET" || -z "$PY_PATH" ]]; then
-  echo "Error: requied option values are missing" >&2
-  exit 1
+# checking the existence of config file
+if [ -z "$CONFIG_FILE" ]; then
+    echo "Error: Configuration file not specified."
+    echo "Usage: $0 <config_file>"
+    exit 1
 fi
 
-# chekcing file does exist
-if [[ ! -f "$SAMPLE" ]]; then
-  echo "Error: File '$SAMPLE' not found." >&2
-  exit 1
+#  checking the existence of config file
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "Error: Configuration file '$CONFIG_FILE' not found."
+    exit 1
 fi
 
-if [[ ! -f "$REF_GENOME" ]]; then
-  echo "Error: File '$REF_GENOME' not found." >&2
-  exit 1
-fi
+# reading confing file
+eval $(awk -F: '
+    /^[[:space:]]*$/ {next}      # skip empty line
+    /^[[:space:]]*#/ {next}      # ignore comment line
 
-if [[ ! -d "$DATA_PATH" ]]; then
-  echo "Error: Directory '$DATA_PATH' not found." >&2
-  exit 1
-fi
+    # remove spaces
+    {gsub(/^[ \t]+|[ \t]+$/, "", $1); gsub(/^[ \t]+|[ \t]+$/, "", $2)}
 
-if [[ ! -f "$NRPH" ]]; then
-  echo "Error: File '$NRPH' not found." >&2
-  exit 1
-fi
-
-if [[ ! -f "$TARGET" ]]; then
-  echo "Error: File '$TARGET' not found." >&2
-  exit 1
-fi
-
-if [[ ! -f "$DFAM_INFO" ]]; then
-  echo "Error: File '$DFAM_INFO' not found." >&2
-  exit 1
-fi
-if [[ ! -f "$ALT_CHR_LIST" ]]; then
-  echo "Error: File '$ALT_CHR_LIST' not found." >&2
-  exit 1
-fi
+    # exporting parameters
+    {print $1"=\"" $2 "\""}
+' "$CONFIG_FILE")
 
 echo "=== creating directories ==="
 mkdir $DATA_PATH/sampledata
@@ -118,7 +88,6 @@ mkdir $DATA_PATH/inMR/genotype/sampledata/$line
 done < $SAMPLE
 
 echo "=== copying and generating files ==="
-cp $PY_PATH/*.py $DATA_PATH/script/
 cp $REF_GENOME $DATA_PATH/reference/reference.fasta
 samtools faidx $DATA_PATH/reference/reference.fasta
 cp $DFAM_INFO $DATA_PATH/dfam_info/
@@ -126,14 +95,14 @@ cp $ALT_CHR_LIST $DATA_PATH/reference/alt_chr_list
 cut -f2 $DFAM_INFO | uniq | sort > $DATA_PATH/dfam_info/target_name.txt
 cut -f3 $DFAM_INFO | uniq | sort > $DATA_PATH/dfam_info/target_class.txt
 echo "=== generating target bed file ==="
-python3 $DATA_PATH/script/wordgrep.py $TARGET $DATA_PATH/dfam_info/target_name.txt $DATA_PATH/dfam_info/target.hits.gz
+python3 $PY_PATH/wordgrep.py $ALL_REPEAT $DATA_PATH/dfam_info/target_name.txt $DATA_PATH/dfam_info/target.hits.gz
 echo "=== generating target bed file: finished ==="
 zcat $DATA_PATH/dfam_info/target.hits.gz | cut -f 1,10,11  > $DATA_PATH/dfam_info/target.hits_convert.bed
-python3 $DATA_PATH/script/convert_bed.py $DATA_PATH/dfam_info/target.hits_convert.bed  $DATA_PATH/dfam_info/target_pre.bed
+python3 $PY_PATH/convert_bed.py $DATA_PATH/dfam_info/target.hits_convert.bed  $DATA_PATH/dfam_info/target_pre.bed
 sort -V -k1,1 -k2,2 $DATA_PATH/dfam_info/target_pre.bed | bedtools merge -d 50 -i - > $DATA_PATH/dfam_info/target.bed
 rm $DATA_PATH/dfam_info/target_pre.bed
-zcat $NRPH | awk 'BEGIN {OFS="\t"} {print $1, $10, $11, $3, $3, $9}'  > $DATA_PATH/dfam_info/hg38.nrph.hits_convert.bed
-python3 $DATA_PATH/script/convert_bed.py  $DATA_PATH/dfam_info/hg38.nrph.hits_convert.bed  $DATA_PATH/dfam_info/nrph.bed
+zcat $NRPH_REPEAT | awk 'BEGIN {OFS="\t"} {print $1, $10, $11, $3, $3, $9}'  > $DATA_PATH/dfam_info/hg38.nrph.hits_convert.bed
+python3 $PY_PATH/convert_bed.py  $DATA_PATH/dfam_info/hg38.nrph.hits_convert.bed  $DATA_PATH/dfam_info/nrph.bed
 sort -V -k1,1 -k2,2 $DATA_PATH/dfam_info/nrph.bed  > $DATA_PATH/dfam_info/nrph.sorted.bed
 bedtools getfasta -fi $DATA_PATH/reference/reference.fasta -name+ -bed $DATA_PATH/dfam_info/nrph.sorted.bed -s -fo $DATA_PATH/check_seq/bwa/subject/nrph.fasta
 bwa index $DATA_PATH/check_seq/bwa/subject/nrph.fasta
