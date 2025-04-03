df75cdeaa965d3567b6f6ca259546c5f6b28fe2c
DW8496 <96272772+DW8496@users.noreply.github.com>
2025-04-03 10:44:55 +0900

    Update preprocess.sh

--- pipeline/preprocess.sh
+++ pipeline/preprocess.sh
@@ -50,28 +50,6 @@ mkdir $DATA_PATH/genotype/allsample
 mkdir $DATA_PATH/01table
 mkdir $DATA_PATH/01table/genotype
 mkdir $DATA_PATH/vcf
-mkdir $DATA_PATH/inMR
-mkdir $DATA_PATH/inMR/sampledata
-mkdir $DATA_PATH/inMR/script
-mkdir $DATA_PATH/inMR/check_seq
-mkdir $DATA_PATH/inMR/check_seq/fq
-mkdir $DATA_PATH/inMR/check_seq/fq/sampledata
-mkdir $DATA_PATH/inMR/check_seq/bwa
-mkdir $DATA_PATH/inMR/check_seq/bwa/result
-mkdir $DATA_PATH/inMR/check_seq/bwa/subject
-mkdir $DATA_PATH/inMR/check_seq/master_file
-mkdir $DATA_PATH/inMR/check_seq/master_file/sampledata
-mkdir $DATA_PATH/inMR/dfam_info
-mkdir $DATA_PATH/inMR/allsample_merge
-mkdir $DATA_PATH/inMR/allsample_merge/sampledata
-mkdir $DATA_PATH/inMR/genotype
-mkdir $DATA_PATH/inMR/genotype/breakpoint
-mkdir $DATA_PATH/inMR/genotype/breakpoint/sampledata
-mkdir $DATA_PATH/inMR/genotype/sampledata
-mkdir $DATA_PATH/inMR/genotype/allsample
-mkdir $DATA_PATH/inMR/01table
-mkdir $DATA_PATH/inMR/01table/genotype
-mkdir $DATA_PATH/inMR/vcf
 mkdir $DATA_PATH/reference
 while read line
 do
@@ -80,11 +58,6 @@ mkdir $DATA_PATH/sampledata/$line/clip
 mkdir $DATA_PATH/sampledata/$line/read_info
 mkdir $DATA_PATH/sampledata/$line/curated_bam
 mkdir $DATA_PATH/genotype/sampledata/$line
-mkdir $DATA_PATH/inMR/sampledata/$line
-mkdir $DATA_PATH/inMR/sampledata/$line/clip
-mkdir $DATA_PATH/inMR/sampledata/$line/read_info
-mkdir $DATA_PATH/inMR/sampledata/$line/curated_bam
-mkdir $DATA_PATH/inMR/genotype/sampledata/$line
 done < $SAMPLE
 
 echo "=== copying and generating files ==="
