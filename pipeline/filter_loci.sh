548121ed75e742e856097d97fa28798785492c5b
nosada17 <35247476+nosada17@users.noreply.github.com>
2026-02-19 17:19:43 +0900

    Update filter_loci.sh

--- pipeline/filter_loci.sh
+++ pipeline/filter_loci.sh
@@ -28,27 +28,23 @@ eval $(awk -F: '
     {print $1"=\"" $2 "\""}
 ' "$CONFIG_FILE")
 
+ACCESSIONLIST="$DATA_PATH/dfam_info/target_class.txt"
+
 date
 echo "=== process7: filtering loci by checking the insertion contents and directions  ==="
-cat $DATA_PATH/check_seq/master_file/sampledata/*strand.csv > $DATA_PATH/check_seq/master_file/allsample_data_merge.csv
+cat $DATA_PATH/check_seq/master_file/sampledata/*strand.tsv > $DATA_PATH/check_seq/master_file/allsample_data_merge.tsv
 sort -k5,5 $DATA_PATH/check_seq/master_file/allsample_data_merge.csv > $DATA_PATH/check_seq/master_file/allsample_data_merge_sort.csv
 paste $DATA_PATH/check_seq/master_file/allsample_data_merge_sort.csv $DATA_PATH/check_seq/bwa/result/mapping_info_sort > $DATA_PATH/check_seq/master_file/allsample_ERVinfo_master.txt
 sort -V -k1,1 -k11,11 $DATA_PATH/check_seq/master_file/allsample_ERVinfo_master.txt > $DATA_PATH/check_seq/master_file/allsample_ERVinfo_master_sort.txt
-cut -f 1,14 $DATA_PATH/check_seq/master_file/allsample_ERVinfo_master_sort.txt > $DATA_PATH/check_seq/master_file/allsample_pos_subject
-awk -F'\t' '{split($2, a, "::"); $2 = a[1]; print $1 "\t" $2}' $DATA_PATH/check_seq/master_file/allsample_pos_subject > $DATA_PATH/check_seq/master_file/allsample_pos_subject_nochr
-python3 $PY_PATH/ERVname_to_class.py $DFAM_INFO $DATA_PATH/check_seq/master_file/allsample_pos_subject_nochr_sed
-python3 $PY_PATH/DFaccession_to_class.py $DFAM_INFO $DATA_PATH/check_seq/master_file/allsample_pos_subject_nochr_class
-python3 $PY_PATH/major_class.py $DATA_PATH/check_seq/master_file/allsample_pos_subject_nochr_class $DATA_PATH/check_seq/master_file/pos_allclass_report $DATA_PATH/check_seq/master_file/pos_majorclass_report
-awk -F'\t' -v threshold="$IDENTITY_THRESHOLD" '$3 >= threshold' "$DATA_PATH/check_seq/master_file/pos_majorclass_report" > "$DATA_PATH/check_seq/master_file/pos_majorclass_${IDENTITY_THRESHOLD//./}up"
-python3 $PY_PATH/extract_wanted_accession.py $DATA_PATH/check_seq/master_file/pos_majorclass_${IDENTITY_THRESHOLD//./}up $ACCESSIONLIST $DATA_PATH/check_seq/master_file/pos_majorclass_${IDENTITY_THRESHOLD//./}up_wanted
-cut -f 1 $DATA_PATH/check_seq/master_file/pos_majorclass_${IDENTITY_THRESHOLD//./}up_wanted | sort -V > $DATA_PATH/check_seq/master_file/pos_majorclass_${IDENTITY_THRESHOLD//./}up_wanted_sort
-cut -f 1 $DATA_PATH/check_seq/master_file/pos_majorclass_${IDENTITY_THRESHOLD//./}up_wanted | sort -V > $DATA_PATH/check_seq/master_file/pos_majorclass_${IDENTITY_THRESHOLD//./}up_wanted_sort
-sort -V -k1,1 $DATA_PATH/check_seq/master_file/pos_majorclass_${IDENTITY_THRESHOLD//./}up_wanted > $DATA_PATH/check_seq/master_file/pos_majorclass_${IDENTITY_THRESHOLD//./}up_wanted_sort_ID
+python3 $PY_PATH/identify_family.py $DATA_PATH/check_seq/bwa/result/allsample_to_DfamERV.all.qnamesort.bam $DATA_PATH/check_seq/master_file/allsample_data_merge.tsv $DATA_PATH/check_seq/master_file/repeat_per_POS_info.tsv
+sort -V -k1,1 $DATA_PATH/check_seq/master_file/repeat_per_POS_info.call_pos.txt > $DATA_PATH/check_seq/master_file/repeat_per_POS_info.call_pos_sorted.txt
+awk -F'\t' ' NR==1 {next} $2!="NA" { split($2, a, "::"); print $1 "\t" a[1] } ' $DATA_PATH/check_seq/master_file/repeat_per_POS_info.call.tsv > $DATA_PATH/check_seq/master_file/allsample_ervname.tsv
+awk -F'\t' '{print $1 "\t" $5}' $DATA_PATH/check_seq/master_file/repeat_per_POS_info.call.tsv > $DATA_PATH/check_seq/master_file/allsample_strand.tsv
+awk -F'\t' 'BEGIN{OFS="\t"} NR==FNR{map[$2]=$3; next} {if($2 in map){$2=map[$2]} else {$2="NA"}; print}' "$DFAM_INFO" "$DATA_PATH/check_seq/master_file/allsample_ervname.tsv" > "$DATA_PATH/check_seq/master_file/allsample_class.tsv"
 cut -f 1,10 $DATA_PATH/check_seq/master_file/allsample_ERVinfo_master_sort.txt > $DATA_PATH/check_seq/master_file/pos_sample
 sort -V -k1,1 -k2,2 $DATA_PATH/check_seq/master_file/pos_sample | uniq > $DATA_PATH/check_seq/master_file/pos_sample_uniq
-cut -f 1,10,11,13 $DATA_PATH/check_seq/master_file/allsample_ERVinfo_master_sort.txt > $DATA_PATH/check_seq/master_file/allsample_POSID_strand_seq
-python3 $PY_PATH/get_strand_majoority.py $DATA_PATH/check_seq/master_file/allsample_POSID_strand_seq $DATA_PATH/check_seq/master_file/allsample_POSID_strand_pass $DATA_PATH/check_seq/master_file/allsample_POSID_strand_filtered
+awk 'NR==FNR {a[$1]; next} ($1 in a)' $DATA_PATH/check_seq/master_file/repeat_per_POS_info.call_pos_sorted.txt $DATA_PATH/check_seq/master_file/pos_sample_uniq > $DATA_PATH/check_seq/master_file/pos_sample_uniq_filtered
 python3 $PY_PATH/make01table.py $DATA_PATH/check_seq/master_file/pos_sample_uniq $SAMPLE $DATA_PATH/01table/allsample_01table.tsv
-python3 $PY_PATH/extract_major_pos.py $DATA_PATH/01table/allsample_01table.tsv $DATA_PATH/check_seq/master_file/pos_majorclass_${IDENTITY_THRESHOLD//./}up_wanted_sort $DATA_PATH/01table/allsample_01table_${IDENTITY_THRESHOLD//./}up.tsv
+python3 $PY_PATH/extract_major_pos.py $DATA_PATH/01table/allsample_01table.tsv $DATA_PATH/check_seq/master_file/repeat_per_POS_info.call_pos_sorted.txt $DATA_PATH/01table/allsample_01table_filtered.tsv
 date
-echo "=== process7: filtering loci by checking the insertion contents and directions: finished extracting loci with ${IDENTITY_THRESHOLD} identity threshold ==="
+echo "=== process7: filtering loci by checking the insertion contents and directions: finished extracting loci ==="
